dcf3291623d9fe54bcbf0ca48c0ed35865b8871a
euvl <yev.vlasenko@gmail.com>
2017-06-20 03:39:15 +0100

    Added putputs for client and server

---
+++ build/webpack.base.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+//  "build:client": "cross-env NODE_ENV=production webpack --config ./build/webpack.client.config.js --progress --hide-modules",
+
+module.exports = {
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: 'index.js',
+    library:'VueJsModal',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: {
+    vue: 'vue'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devtool: '#source-map',
+  plugins: [
+    new UglifyJSPlugin()
+  ]
+}
---
+++ build/webpack.server.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const base = require('./webpack.base.config')
+
+module.exports = merge(base, {
+  target: 'node',
+  output: {
+    filename: 'ssr.index.js'
+  }
+})
