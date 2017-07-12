981a0c8fdb6ce7530befd54d54a0bac971bdd2c3
euvl <yev.vlasenko@gmail.com>
2017-07-12 11:17:33 +0100

    Added test case for PR #46

---
+++ demo/client_side_rendering/src/components/SizeModal.vue
@@ -0,0 +1,15 @@
+<template> 
+  <modal name="size-modal"
+         transition="nice-modal-fade"
+         :min-width="200"
+         :min-height="200" 
+         :adaptive="true"
+         width="60%"
+         height="60%">
+    <div style="height: 100%; box-sizing: border-box; padding: 10px; font-size: 13px; overflow: auto">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dictum purus egestas libero ornare venenatis. Maecenas pharetra tortor eu tortor imperdiet, a faucibus quam finibus. Nulla id lacinia quam. Praesent imperdiet sed magna non finibus. Aenean blandit, mauris vitae lacinia rutrum, nunc mi scelerisque sem, in laoreet sem lectus ut orci. Ut egestas nulla in vehicula feugiat. Vivamus tincidunt nisi vel risus dictum suscipit. Nulla id blandit mi, vulputate aliquam enim.
+
+    Praesent sodales diam a tellus imperdiet condimentum. Mauris malesuada libero elit, sed tincidunt velit consectetur sit amet. Praesent consequat lobortis pellentesque. Donec pellentesque eros sit amet arcu bibendum, vel pharetra purus sodales. Morbi aliquam ac augue eu aliquam. Cras id justo sit amet tortor luctus lobortis. Vestibulum risus urna, cursus vitae feugiat non, tincidunt non massa.
+    </div>
+  </modal>
+</template>
