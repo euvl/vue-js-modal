a4067d7b4c9a3161693248cca9f3aa97fa2ee58d
yev <yev.vlasenko@gmail.com>
2018-10-27 20:27:00 +0100

    Minor cleanup, pushed package lock

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-const webpack = require('webpack')
 const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
 
--- build/webpack.ssr-no-css.config.js
+++ build/webpack.ssr-no-css.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const merge = require('webpack-merge')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
