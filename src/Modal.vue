bb5cde9afd370302b5266e0bf8a01a62950eec16
Yev Vlasenko <yev.vlasenko@gmail.com>
2020-02-08 22:28:00 +0000

    Revert "Revert "Honor minWidth and minHeight on adaptive (#465)""
    This reverts commit 0af37aa359f954068d0ef2a8f0935a6b387c62eb.

--- src/Modal.vue
+++ src/Modal.vue
@@ -296,7 +296,7 @@ export default {
         ? window.width / 100 * modal.width
         : modal.width
 
-      const max = Math.min(window.width, maxWidth)
+      const max = Math.max(minWidth, Math.min(window.width, maxWidth))
 
       return adaptive
         ? inRange(minWidth, max, value)
@@ -309,7 +309,7 @@ export default {
      * Returns modal.renderedHeight if height set as "auto"
      */
     trueModalHeight () {
-      const { window, modal, isAutoHeight, adaptive, maxHeight } = this
+      const { window, modal, isAutoHeight, adaptive, minHeight, maxHeight } = this
 
       const value = modal.heightType === '%'
         ? window.height / 100 * modal.height
@@ -320,10 +320,10 @@ export default {
         return this.modal.renderedHeight
       }
 
-      const max = Math.min(window.height, maxHeight)
+      const max = Math.max(minHeight, Math.min(window.height, maxHeight))
 
       return adaptive
-        ? inRange(this.minHeight, max, value)
+        ? inRange(minHeight, max, value)
         : value
     },
     /**
