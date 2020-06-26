fd82808ffba6a8491f8466a12554b6fd43f57046
Yev <yev.dev@gmail.com>
2020-06-26 19:29:37 +0100

    Added some docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,20 @@
+const sidebar = require('./sidebar')
+
 module.exports = {
   base: '/vue-js-modal/',
   title: 'Vue.js Modal',
-  description: 'Simple, flexible, Vue.js modal plugin'
+  description: 'Simple, flexible, Vue.js modal plugin',
+  themeConfig: {
+    displayAllHeaders: false,
+
+    sidebar,
+    nav: [
+      {
+        text: 'Sponsorship',
+        link: 'https://github.com/sponsors/euvl'
+      },
+      { text: 'Github', link: 'https://github.com/euvl/vue-js-modal' },
+      { text: 'Examples', link: '/examples/' }
+    ]
+  }
 }
