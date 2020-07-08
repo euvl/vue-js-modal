2e4f9592d62c8bb9dbf5a62effee56d9bc67d22a
David Rouyer <rouyer.david@gmail.com>
2020-07-08 18:58:54 +0200

    feat: add client nocss bundle

---
+++ build/webpack.client-no-css.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, '../src/index.js'),
+  devtool: 'source-map',
+  output: {
+    library: 'vue-js-modal',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: 'index.nocss.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({
+        canPrint: true
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ]
+}
